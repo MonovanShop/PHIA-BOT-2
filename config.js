df379e5c78562d237654136c906e6890827c6e13
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2026-04-14 18:04:51 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
-module.exports = {
-  botName: "TiendaBot",
-  ownerName: "Christopher",
-  ownerNumber: "5210000000000", // cambia esto
-  prefix: ".",
+export default {
+  botName: "GM LUXURY BOT",
+  storeName: "GM LUXURY",
+  prefixes: [".", "/"],
+  owners: [
+    {
+      name: "Alexita (LID)",
+      number: "136640240578734", // El ID que sale en tu captura
+      role: "owner"
+    },
+    {
+      name: "Daniel",
+      number: "61697960194286",
+      role: "owner"
+    }
+  ]
 };
